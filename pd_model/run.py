2ec9afc6afca4f4dee97ae55c313f2dfbd49cc82
mmaynard-code <m.maynard.home@gmail.com>
2022-10-09 13:49:30 +0100

    clean up run.py

--- pd_model/run.py
+++ pd_model/run.py
@@ -5,10 +5,6 @@ import mesa
 import pandas as pd
 from model import SimpleModel
 
-# empty_model = SimpleModel(8, 2, 4, "gossip")
-# empty_model.step()
-# empty_model.step()
-
 if __name__ == "__main__":
     freeze_support()
     results = mesa.batch_run(
@@ -28,8 +24,5 @@ if __name__ == "__main__":
     )
 
     results_df = pd.DataFrame(results)
-
-    print(results_df[(results_df["Step"] == 10) & (results_df["AgentID"] == 0)])
-    # print(results_df)
 
     results_df.to_csv("results.csv")
