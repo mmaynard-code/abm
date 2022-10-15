d2b82862f004609aad7eb73cc525c2a1211dce93
mmaynard-code <m.maynard.home@gmail.com>
2022-10-15 23:16:57 +0100

    finalise output dfs and pipeline for abm

--- experimental_analysis/analysis.R
+++ experimental_analysis/analysis.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(igraph)
 library(ggbeeswarm)
 library(stringr)
+library(glue)
 
 df <- read.csv("experimental_data.csv") %>%
   mutate(treatment_ref = case_when(
@@ -31,48 +32,142 @@ opponent_2_data <- df %>%
     player_decision = player_decision2,
     opponent_pre_pd_reputation = opponent_2_pre_pd_reputation)
 
-neighbour_1_data <- df %>%
-  select(
-    starts_with("neighbour_1"),
-    treatment_ref) %>%
-  rename_all(
-    ~str_replace(.x,"neighbour_1_","neighbour_"))
-neighbour_2_data <- df %>%
-  select(
-    starts_with("neighbour_2"),
-    treatment_ref) %>%
-  rename_all(
-    ~str_replace(.x,"neighbour_2_","neighbour_"))
-neighbour_3_data <- df %>%
-  select(
-    starts_with("neighbour_3"),
-    treatment_ref) %>%
-  rename_all(
-    ~str_replace(.x,"neighbour_3_","neighbour_"))
-
-
-
-share_decision_data <- bind_rows(
-  neighbour_1_data,
-  neighbour_2_data,
-  neighbour_3_data
-  )
-
 pd_decision_data <- bind_rows(
   opponent_1_data,
   opponent_2_data
+  ) %>%
+  mutate(
+    player_decision = case_when(
+      player_decision == "X" ~ "Cooperate",
+      player_decision == "Y" ~ "Defect",
+    )
   )
 
+for (i in seq(1,3,1)) {
+  df_to_bind <- df %>%
+    select(
+      starts_with(glue("neighbour_{i}")),
+      treatment_ref)  %>%
+    rename_all(
+      ~str_replace(.x,glue("neighbour_{i}_"),"neighbour_")) %>%
+    mutate(
+      neighbour_share_decision = case_when(
+        neighbour_share_decision == "Yes" ~ "Share",
+        neighbour_share_decision == "No" ~ "Won't share",
+        neighbour_share_decision == "Sharing Not Possible" ~ "Can't share",
+      )
+    )
+  if (i == 1) {
+    share_decision_data <- df_to_bind
+  } else {
+    share_decision_data <- bind_rows(share_decision_data, df_to_bind)
+  }
+}
+
+for (i in seq(1,16,1)) {
+  update_df <- df %>%
+    select(
+      starts_with(glue("other_player_{i}_"))) %>%
+    rename_all(
+      ~str_replace(.x,glue("other_player_{i}_"),"")
+    )
+  rep_df <- df %>%
+    select(
+      starts_with("neighbour_"),
+      -!ends_with("reputation"),
+      treatment_ref
+    )
+  combined_df <- bind_cols(update_df, rep_df)
+  for (j in seq(1,3,1)) {
+    neighbour_df <- combined_df %>%
+      select(
+        starts_with(glue("neighbour_{j}_"))) %>%
+      rename_all(
+        ~str_replace(.x,glue("neighbour_{j}_"),"neighbour_")
+      )
+    other_df <- combined_df %>%
+      select(
+        -starts_with(glue("neighbour_")))
+    recombined_df <- bind_cols(neighbour_df, other_df)
+    if (j == 1) {
+      df_to_bind <- recombined_df
+    } else {
+      df_to_bind <- bind_rows(df_to_bind, recombined_df)
+    }
+  }
+  if (i == 1) {
+    update_decision_data <- df_to_bind
+  } else {
+    update_decision_data <- bind_rows(update_decision_data, df_to_bind)
+  }
+}
 
 pd_decision_data_for_abm <- pd_decision_data %>%
-  group_by(treatment_ref, opponent_pre_pd_reputation, player_decision) %>%
+  group_by(
+    treatment_ref,
+    opponent_pre_pd_reputation,
+    player_decision
+    ) %>%
   summarise(n = n()) %>%
   mutate(freq = n / sum(n)) %>%
   select(-n)
 
 share_decision_data_for_abm <- share_decision_data %>%
   filter(neighbour_share_decision != "Sharing Not Possible") %>%
-  group_by(treatment_ref, neighbour_post_pd_reputation, neighbour_share_decision) %>%
+  group_by(
+    treatment_ref,
+    neighbour_post_pd_reputation,
+    neighbour_share_decision
+    ) %>%
   summarise(n = n()) %>%
   mutate(freq = n / sum(n)) %>%
   select(-n)
+
+update_decision_data_neighbour_rep_for_abm <- update_decision_data %>%
+  filter(gossip_available >= 1 & neighbour_gossip_new_flag == "True") %>%
+  group_by(
+    treatment_ref,
+    neighbour_post_pd_reputation,
+    gossip_change_flag
+    ) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  select(-n)
+
+update_decision_data_filtered <- update_decision_data %>%
+  mutate(
+    gossip_difference = abs(neighbour_gossip - pre_pd_reputation)
+  ) %>%
+  filter(
+    gossip_available >= 1
+    & !is.na(gossip_difference)
+    & gossip_difference > 0
+    & (neighbour_pre_gossip_consensus > 0 | neighbour_gossip_new_flag == 'True')
+    )
+
+update_decision_data_gossip_rep_for_abm <- update_decision_data_filtered %>%
+  group_by(
+    treatment_ref,
+    neighbour_gossip,
+    gossip_change_flag
+  ) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  select(-n)
+
+update_decision_data_neighbour_rep_for_abm <- update_decision_data_filtered %>%
+  group_by(
+    treatment_ref,
+    neighbour_post_pd_reputation,
+    gossip_change_flag
+  ) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  select(-n)
+
+write.csv(pd_decision_data, "pd_decision_data.csv")
+write.csv(share_decision_data, "share_decision_data.csv")
+write.csv(update_decision_data, "update_decision_data.csv")
+write.csv(update_decision_data_filtered, "update_decision_data_filtered.csv")
+write.csv(update_decision_data_neighbour_rep_for_abm, "update_decision_data_neighbour_rep.csv")
+write.csv(update_decision_data_gossip_rep_for_abm, "update_decision_data_gossip_rep.csv")
--- experimental_analysis/figures.py
+++ experimental_analysis/figures.py
@@ -11,51 +11,116 @@ pd_decision_data = pd.read_csv("pd_decision_data.csv")
 pd_decision_fig = px.histogram(
     pd_decision_data.query("opponent_pre_pd_reputation != -1"),
     x="opponent_pre_pd_reputation",
+    range_y=[0, 2000],
     color="player_decision",
-    barnorm="percent",
+    # barnorm="percent",
     facet_col="treatment_ref",
     marginal="box",
     category_orders=dict(
-        player_decision=["X", "Y"],
+        player_decision=["Cooperate", "Defect"],
         opponent_pre_pd_reputation=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         treatment_ref=["Circle Network", "Transitive", "Small World"],
     ),
+    labels={
+        "player_decision": "Game Decision",
+        "opponent_pre_pd_reputation": "Opponent Reputation",
+        "treatment_ref": "Network Type",
+    },
+    title="Player Game Decision by Opponent Reputation",
 )
 pd_decision_fig.update_xaxes(type="category")
 pd_decision_fig.show()
-pd_decision_no_rep_fig = px.histogram(
-    pd_decision_data.query("opponent_pre_pd_reputation == -1"),
-    x="opponent_pre_pd_reputation",
-    color="player_decision",
-    barnorm="percent",
-    facet_col="treatment_ref",
-    marginal="box",
-    category_orders=dict(
-        player_decision=["X", "Y"],
-        # opponent_pre_pd_reputation=[0,1,2,3,4,5,6,7,8,9,10],
-        treatment_ref=["Circle Network", "Transitive", "Small World"],
-    ),
-)
-pd_decision_no_rep_fig.update_xaxes(type="category")
-pd_decision_no_rep_fig.show()
+
+# pd_decision_no_rep_fig = px.histogram(
+#     pd_decision_data.query("opponent_pre_pd_reputation == -1"),
+#     x="opponent_pre_pd_reputation",
+#     color="player_decision",
+#     barnorm="percent",
+#     facet_col="treatment_ref",
+#     marginal="box",
+#     category_orders=dict(
+#         player_decision=["X", "Y"],
+#         # opponent_pre_pd_reputation=[0,1,2,3,4,5,6,7,8,9,10],
+#         treatment_ref=["Circle Network", "Transitive", "Small World"],
+#     ),
+# )
+# pd_decision_no_rep_fig.update_xaxes(type="category")
+# pd_decision_no_rep_fig.show()
 
 share_decision_data = pd.read_csv("share_decision_data.csv")
 share_decision_fig = px.histogram(
     share_decision_data,
     x="neighbour_post_pd_reputation",
-    # range_x=[0,10],
+    range_y=[0, 2000],
     color="neighbour_share_decision",
-    barnorm="percent",
+    # barnorm="percent",
     facet_col="treatment_ref",
     marginal="box",
     category_orders=dict(
-        neighbour_share_decision=["Yes", "No", "Sharing Not Possible"],
+        neighbour_share_decision=["Share", "Won't share", "Can't share"],
         treatment_ref=["Circle Network", "Transitive", "Small World"],
         neighbour_post_pd_reputation=[0, 1, 2, 3, 4, -1, 5, 6, 7, 8, 9, 10],
         neighbour_share_number=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         neighbour_share_low=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         neighbour_share_high=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     ),
+    labels={
+        "neighbour_share_decision": "Share Decision",
+        "neighbour_post_pd_reputation": "Neighbour Reputation",
+        "treatment_ref": "Network Type",
+    },
+    title="Player Share Decision by Neighbour Reputation",
 )
 share_decision_fig.update_xaxes(type="category")
 share_decision_fig.show()
+
+
+update_decision_data = pd.read_csv("update_decision_data_filtered.csv")
+print(update_decision_data["neighbour_gossip_new_flag"].unique())
+update_decision_neighbour_reputation_fig = px.histogram(
+    update_decision_data,
+    x="neighbour_post_pd_reputation",
+    range_y=[0, 2000],
+    color="gossip_change_flag",
+    # barnorm="percent",
+    facet_col="treatment_ref",
+    marginal="box",
+    category_orders=dict(
+        gossip_change_flag=[True, False],
+        treatment_ref=["Circle Network", "Transitive", "Small World"],
+        neighbour_post_pd_reputation=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        neighbour_gossip=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    ),
+    labels={
+        "gossip_change_flag": "Update Decision",
+        "neighbour_post_pd_reputation": "Neighbour Reputation",
+        "treatment_ref": "Network Type",
+    },
+    title="Player Update Decision by Neighbour Reputation",
+)
+update_decision_neighbour_reputation_fig.update_xaxes(type="category")
+update_decision_neighbour_reputation_fig.show()
+
+update_decision_neighbour_gossip_fig = px.histogram(
+    update_decision_data,
+    x="neighbour_gossip",
+    range_y=[0, 2000],
+    color="gossip_change_flag",
+    # barnorm="percent",
+    facet_col="treatment_ref",
+    marginal="box",
+    category_orders=dict(
+        gossip_change_flag=[True, False],
+        treatment_ref=["Circle Network", "Transitive", "Small World"],
+        neighbour_post_pd_reputation=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        neighbour_gossip=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    ),
+    labels={
+        "gossip_change_flag": "Update Decision",
+        "neighbour_gossip": "Neighbour Gossip Score",
+        "treatment_ref": "Network Type",
+    },
+    title="Player Update Decision by Neighbour Gossip Score Received",
+)
+update_decision_neighbour_gossip_fig.update_xaxes(type="category")
+update_decision_neighbour_gossip_fig.show()
